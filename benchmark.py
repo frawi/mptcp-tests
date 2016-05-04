c9f624cfeb66ff8ee2311f20fabfe1adbcde23a3
Frank Wilhelm <frank.wilhelm@tu-dresden.de>
2016-05-04 15:40:58 +0200

    New topology now enables better MPTCP benchmarking

--- benchmark.py
+++ benchmark.py
@@ -8,23 +8,27 @@ def create_network(paths, bw, loss, latency):
 
     net = Mininet(controller=Controller, link=TCLink, host=CPULimitedHost)
 
+    # since we need a switch we also need a controller
+    c0 = net.addController('c0')
+    s1 = net.addSwitch('s1')
+
     # create virtual hosts
     h1 = net.addHost('h1', cpu=40)
     h2 = net.addHost('h2', cpu=40)
 
-    # create the first link
-    info("Create link h1 <-> h2 ")
-    li = net.addLink(h1, h2, loss=loss, bw=bw, delay=latency)
-    li.intf1.setIP("10.0.0.1", 24)
-    li.intf2.setIP("10.0.0.2", 24)
+    info("Create link s1 <-> h2 ")
+    li = net.addLink(s1, h2)
+    info("\n")
+
+    info("Create link h1 <-> s1 ")
+    li = net.addLink(h1, s1, loss=loss, bw=bw, delay=latency)
     info("\n")
 
     # create the links
     for i in range(paths-1):
-        info("Create link h1 <-> h2 ")
-        li = net.addLink(h1, h2, loss=loss, bw=bw, delay=latency)
+        info("Create link h1 <-> s1 ")
+        li = net.addLink(h1, s1, loss=loss, bw=bw, delay=latency)
         li.intf1.setIP("192.168.%d.1"%i, 24)
-        li.intf2.setIP("192.168.%d.2"%i, 24)
         info("\n")
 
     return net
@@ -35,7 +39,7 @@ def set_route(intf, gateway, table):
     netnum = reduce(lambda l, r: (l<<8) + int(r), intf.ip.split("."), 0) >> shift << shift
     netip = ".".join([str((netnum>>(24-i*8))&0xff) for i in range(4)])
     intf.cmd("ip route add %s/%s dev %s scope link table %d" % (netip, intf.prefixLen, intf.name, table), shell=True)
-    intf.cmd("ip route add default via %s dev %s table %d" % (gateway, intf.name, table), shell=True)
+    intf.cmd("ip route add default dev %s table %d" % (intf.name, table), shell=True)
 
 def sysctl(name, value):
     from subprocess import check_output
@@ -48,26 +52,22 @@ def set_parameter(name, value):
     return name + " = " + check_output(["cat", name])
 
 def setup(net):
-    for h in [h1, h2]:
-        for i in h.intfs:
-            h.intfs[i].ifconfig("down")
+    h1 = net["h1"]
+    h2 = net["h2"]
+
+    for i in h1.intfs:
+        h1.intfs[i].ifconfig("down")
     sleep(1)
 
     for i in h1.intfs:
         intf1 = h1.intfs[i]
-        intf2 = h2.intfs[i]
 
         intf1.ifconfig("up")
         intf1.cmd("ip link set dev %s multipath on", intf1.name)
-        set_route(intf1, intf2.ip, i+1)
-
-        intf2.ifconfig("up")
-        intf2.cmd("ip link set dev %s multipath on", intf2.name)
-        set_route(intf2, intf1.ip, i+1)
+        set_route(intf1, h2.intfs[0].ip, i+1)
 
         if i == 0:
-            h1.cmd("ip route add default scope global nexthop via %s dev %s" % (intf2.ip, intf1.name), shell=True)
-            h2.cmd("ip route add default scope global nexthop via %s dev %s" % (intf1.ip, intf2.name), shell=True)
+            h1.cmd("ip route add default scope global nexthop via %s dev %s" % (h2.intfs[0].ip, intf1.name), shell=True)
 
     sys.stderr.write("h1: ip rule show\n" + h1.cmdPrint("ip rule show"))
     sys.stderr.write("h1: ip route\n" + h1.cmdPrint("ip route"))
@@ -75,17 +75,23 @@ def setup(net):
     for i in h1.intfs:
         sys.stderr.write(("h1: ip route show table %d\n"%(i+1)) + h1.cmdPrint("ip route show table %d" % (i+1)))
 
+    h2.cmd("ip route add default dev %s" % h2.defaultIntf().name)
+
     sys.stderr.write("h2: ip rule show\n" + h2.cmdPrint("ip rule show"))
     sys.stderr.write("h2: ip route\n" + h2.cmdPrint("ip route"))
 
     for i in h2.intfs:
         sys.stderr.write(("h2: ip route show table %d\n"%(i+1)) + h2.cmdPrint("ip route show table %d" % (i+1)))
 
-def start_bwm(node):
+def start_bwm(node, filename=None):
     from mininet.term import makeTerm
 
     cmd = ["bwm-ng", "-u", "bits"]
-    makeTerm(node, cmd="bash -c '%s || read'"% " ".join(cmd))
+    if filename:
+        return node.popen(["-o", "csv", "-F", filename, "-T", "sum"])
+    else:
+        makeTerm(node, cmd="bash -c '%s || read'"% " ".join(cmd))
+        return None
 
 def start_htop(node):
     from mininet.term import makeTerm
@@ -95,6 +101,15 @@ def start_htop(node):
 
 def start_tcpdump(node):
     return node.popen(["tcpdump", "-i", "any", "-s", "65535", "-w", node.name+".pcap"])
+
+def pingall(*nodes):
+    for s in nodes:
+        for d in nodes:
+            if s == d:
+                continue
+            for i in s.intfs:
+                for j in d.intfs:
+                    sys.stderr.write(s.cmdPrint(["ping", "-I", s.intfs[i].name, "-c", "4", d.intfs[j].ip]))
 
 if __name__ == '__main__':
     from time import sleep
@@ -106,7 +121,9 @@ if __name__ == '__main__':
             help="Run the tunnel in xterm. This makes the stdout of nctun visible")
     parser.add_argument("--cli", action='store_true',
             help="Run the mininet CLI instead of the benchmark")
-    parser.add_argument("--bwm", action='store_true',
+    parser.add_argument("--bwm", default=None,
+            help="Run a bandwidth monitor on the switch and save to a csv file")
+    parser.add_argument("--bwm-term", action='store_true',
             help="Run a bandwidth monitor on the nodes")
     parser.add_argument("--htop", action='store_true',
             help="Run a htop on the nodes")
@@ -166,12 +183,20 @@ if __name__ == '__main__':
     net = create_network(paths=args.paths, bw=args.bw, loss=args.loss, latency=args.latency)
     net.start()
 
+    s1 = net['s1']
     h1 = net['h1']
     h2 = net['h2']
 
     sleep(1)
+    setup(net)
+    pingall(h1, h2)
+
+    procs = []
 
     if args.bwm:
+        procs.append(start_bwm(s1, args.bwm))
+
+    if args.bwm_term:
         start_bwm(h1)
         start_bwm(h2)
 
@@ -180,11 +205,10 @@ if __name__ == '__main__':
         start_htop(h2)
 
     if args.tcpdump:
-        h1dump = start_tcpdump(h1)
-        h2dump = start_tcpdump(h2)
+        procs.append(start_tcpdump(h1))
+        procs.append(start_tcpdump(h2))
 
     sleep(2)
-    setup(net)
 
     if args.cli:
         from mininet.cli import CLI
@@ -195,11 +219,10 @@ if __name__ == '__main__':
             result = net.iperf(seconds=args.time, fmt="m")
             print "%s %s" % (result[0], result[1])
 
-    if args.tcpdump:
-        h1dump.terminate()
-        h1dump.wait()
-        h2dump.terminate()
-        h2dump.wait()
+    for p in procs:
+        if p:
+            p.terminate()
+            p.wait()
 
     net.stop()
 
